c0eab336c126ec6eed65fae29d5d23350e81386b
Brandon Romaine <brandonmromaine@gmail.com.com>
2022-04-08 11:43:40 -0600

    Refactor of JS and CSS

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
-let play1;
-let play2;
-let play1Score = 0;
-let play2Score = 0;
-let play2Cap;
-
-function game(){
-
-    // computer choses its weapon
-function computerPlay() {
+let computerSelection;
+let playerSelection;
+let ComputerScore = 0;
+let player1Score = 0;
 
 
-    let val = (Math.floor(Math.random() * 3));
-    if (val === 0) {
-        play1 = "Rock"
-    } else if (val === 1) {
-        play1 = "Paper"
-    } else if (val === 2) {
-        play1 = "Scissors"
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+
+        // player 1 choses weapon
+        playerSelection = prompt('pick');
+
+
+        function computerPlay() {
+            // create random num between 0-2
+            let randomNum = Math.floor(Math.random() * 3);
+            //    convert randomNum to weapon
+            if (randomNum === 0) {
+                computerSelection = 'Rock';
+            } else if (randomNum === 1) {
+                computerSelection = 'Paper';
+            } else if (randomNum === 2) {
+                computerSelection = 'Scissors'
+            }
+            console.log(computerSelection);
+        }
+        computerPlay();
+
+        // evaluation of players weapons returning a winner
+        function playRound(playerSelection, computerSelection) {
+            if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+                console.log('Draw');
+            } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+                console.log('Paper beats Rock. Computer wins round!');
+                ComputerScore++;
+            } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+                console.log('Rock beats Scissors. Player 1 wins!!');
+                player1Score++;
+            } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
+                console.log('Draw');
+            } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+                console.log('Paper beats Rock. Player 1 wins!');
+                player1Score++;
+            } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+                console.log('Scissors beats Paper. Computer wins!');
+                ComputerScore++;
+            } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+                console.log('Draw');
+            } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+                console.log('Rock beats Scissors. Computer wins!');
+                ComputerScore++;
+            } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+                console.log('Scissors beats Paper. Player 1! wins!');
+                player1Score++
+            } else {
+                console.log('error');
+            }
+
+        }
+
+
+        playRound(playerSelection, computerSelection);
+
     }
-    console.log(play1);
-    return play1;
-
+    console.log('game over');
+    console.log(player1Score, ComputerScore);
 }
-computerPlay();
-
-
-
-function player2Play() {
-    play2 = prompt("Rock Paper, or Scissors");
-    play2Cap = play2.charAt(0).toUpperCase() + play2.slice(1).toLowerCase();
-    play2 = play2Cap
-    console.log(play2);
-    evaluate(play1, play2);
-}
-
-player2Play();
-
-function evaluate(play1, play2) {
-
-    if (play1 === "Rock" && play2 === "Scissors") {
-        alert("Player 1 wins");
-        play1Score ++;
-    } else if (play1 === "Rock" && play2 === "Rock") {
-        alert("Draw");
-    } else if (play1 === "Paper" && play2 === "Scissors") {
-        alert("Player 2 wins");
-        play2Score ++;
-    } else if (play1 === "Paper" && play2 === "Paper") {
-        alert("Draw");
-    } else if (play1 === "Scissors" && play2 === "Paper") {
-        alert("Player 1 Wins!!");
-        play1Score ++;
-    } else if (play1 === "Scissors" && play2 === "Scissors") {
-        alert("Draw");
-    } else {
-        alert("Player 2 Wins!!");
-        play2Score ++;
-    }
-}
-
-}
-
-// for(let i=0; i<=4; i++){
-// game();
-
-
-// if(i === 4){
-//     alert(`Player 1 score is ${play1Score}, Player 2 score is ${play2Score}`);
-
-// }}
+game();
